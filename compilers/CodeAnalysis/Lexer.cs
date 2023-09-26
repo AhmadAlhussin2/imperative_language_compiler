963d125f33d933fa767a109e89fffbe4d73b4eb2
Ahmad Alhussin <ahmad.alhusein2@gmail.com>
2023-09-26 17:32:28 +0300

    added variables and keywords

--- compilers/CodeAnalysis/Lexer.cs
+++ compilers/CodeAnalysis/Lexer.cs
@@ -28,6 +28,42 @@ namespace compilers.CodeAnalysis
             _position++;
         }
 
+        private SyntaxToken ReadVarOrKeyword(int start)
+        {
+            while (char.IsDigit(Current) || char.IsLetter(Current) || Current == '_')
+            {
+                Next();
+            }
+            var identifier = _text[start.._position];
+            var kind = identifier switch
+            {
+                "and" => SyntaxKind.AndKeyword,
+                "or" => SyntaxKind.OrKeyword,
+                "xor" => SyntaxKind.XorKeyword,
+                "if" => SyntaxKind.IfKeyword,
+                "is" => SyntaxKind.IsKeyword,
+                "integer" => SyntaxKind.IntegerKeyword,
+                "real" => SyntaxKind.RealKeyword,
+                "var" => SyntaxKind.VarKeyword,
+                "type" => SyntaxKind.TypeKeyword,
+                "boolean" => SyntaxKind.BooleanKeyword,
+                "true" => SyntaxKind.TrueKeyword,
+                "false" => SyntaxKind.FalseKeyword,
+                "record" => SyntaxKind.RecordKeyword,
+                "end" => SyntaxKind.EndKeyword,
+                "array" => SyntaxKind.ArrayKeyword,
+                "while" => SyntaxKind.WhileKeyword,
+                "loop" => SyntaxKind.LoopKeyword,
+                "for" => SyntaxKind.ForKeyword,
+                "reverse" => SyntaxKind.ReverseKeyword,
+                "then" => SyntaxKind.ThenKeyword,
+                "else" => SyntaxKind.ElseKeyword,
+                "routine" => SyntaxKind.RoutineKeyword,
+                "not" => SyntaxKind.NotKeyword,
+                _ => SyntaxKind.IdentifierToken,
+            };
+            return new SyntaxToken(kind, start, identifier, null);
+        }
         private SyntaxToken ReadNumber(int start)
         {
             while (char.IsDigit(Current))
@@ -60,21 +96,62 @@ namespace compilers.CodeAnalysis
                 var number = _text[start.._position];
                 return new SyntaxToken(SyntaxKind.WhiteSpace, start, number, null);
             }
-            if (Current == '+')
-                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
-            else if (Current == '-')
-                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
-            else if (Current == '*')
-                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
-            else if (Current == '/')
-                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
-            else if (Current == '(')
-                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
-            else if (Current == ')')
-                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
-
-            _errors.Add($"Error bad character in the input: '{Current}'");
-            return new SyntaxToken(SyntaxKind.UnknowToken, _position++, _text.Substring(_position - 1, 1), null);
+            switch (Current)
+            {
+                case '+':
+                    return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+                case '-':
+                    return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+                case '*':
+                    return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+                case '/':
+                    _position++;
+                    if (Current == '=')
+                        return new SyntaxToken(SyntaxKind.NotEqualToken, _position++, "/=", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.NegationToken, _position++, "/", null);
+                case '(':
+                    return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+                case ')':
+                    return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                case '[':
+                    return new SyntaxToken(SyntaxKind.OpenSquareBracketToken, _position++, "[", null);
+                case ']':
+                    return new SyntaxToken(SyntaxKind.CloseSquareBracketToken, _position++, "]", null);
+                case '<':
+                    _position++;
+                    if (Current == '=')
+                        return new SyntaxToken(SyntaxKind.LessThanOrEqualToken, _position++, "<=", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.LessThanToken, _position, "<", null);
+                case '>':
+                    _position++;
+                    if (Current == '=')
+                        return new SyntaxToken(SyntaxKind.GreaterThanOrEqualToken, _position++, ">=", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.GreaterThanToken, _position, ">", null);
+                case '%':
+                    return new SyntaxToken(SyntaxKind.ModuloToken, _position++, "%", null);
+                case ':':
+                    _position++;
+                    if (Current == '=')
+                        return new SyntaxToken(SyntaxKind.AssignmentToken, _position++, ":=", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.ColonToken, _position, ":", null);
+                case '=':
+                    return new SyntaxToken(SyntaxKind.EqualToken, _position++, "=", null);
+                case '.':
+                    _position++;
+                    if (Current == '.')
+                        return new SyntaxToken(SyntaxKind.RangeToken, _position++, "..", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.DotToken, _position, ".", null);
+                default:
+                    if (Current == '_' || char.IsLetter(Current))
+                        return ReadVarOrKeyword(start);
+                    _errors.Add($"Error bad character in the input: '{Current}'");
+                    return new SyntaxToken(SyntaxKind.UnknowToken, _position++, _text.Substring(_position - 1, 1), null);
+            }
         }
 
     }
